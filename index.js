98476bbff781c0644683af3a431785a13f214a06
Flacid Papa <travisroske@gmail.com>
2022-12-29 23:41:01 -0800

    Video Server

---
+++ index.js
@@ -0,0 +1,40 @@
+"use strict"
+const express = require("express");
+const app = express();
+const fs = require("fs");
+require("dotenv").config()
+const PORT = process.env.PORT || 4040;
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+app.get("/index.css", (req, res) => {
+    res.sendFile(__dirname + "/index.css")
+})
+
+app.get("/video", (req, res) => {
+    const range = req.headers.range;
+    if(!range) {
+        res.status(400).send(`Request requires "Range" header`)
+    }
+    const vidPath = "sampleVideo.mp4";
+    const vidSize = fs.statSync("sampleVideo.mp4").size;
+    const chunkSize = 10 ** 6;
+    const start = Number(range.match(/\d+/g))
+    const end = Math.min(start + chunkSize, vidSize - 1);
+    const sentChunkSize = end - start + 1;
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${vidSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": sentChunkSize,
+        "Content-Type": "video/mp4"
+    }
+    res.writeHead(206, headers)
+    const vStream = fs.createReadStream(vidPath, { start, end }); //they have to be named start and end, or use { start: var, end: endvar}...
+    vStream.pipe(res);
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
